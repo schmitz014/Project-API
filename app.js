c80693cf741a3980bf681f8faa972ebce0c75b18
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-07-21 08:34:29 -0300

    Creating a requisition

---
+++ app.js
@@ -0,0 +1,20 @@
+// inicial config
+
+const express = require('express');
+const app = express();
+
+// reading JSON configuration / middleware
+app.use(express.urlencoded(
+    {extended: true}
+ )
+);
+
+app.use(express.json());
+
+//endpoint
+app.get('/', (req, res) => {
+    res.json({message: "Express is up!"})
+});
+
+//port
+app.listen(3000)
