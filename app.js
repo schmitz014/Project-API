a0e4768497aa17826789cc5ca8b27f147c7bde31
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-07-21 11:09:45 -0300

    Connecting to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-// inicial config
-
+// initial config
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 // reading JSON configuration / middleware
@@ -17,4 +18,16 @@ app.get('/', (req, res) => {
 });
 
 //port
-app.listen(8080)
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+mongoose
+ .connect(
+  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.5u8qicb.mongodb.net/?retryWrites=true&w=majority`
+ )
+ .then(() => {
+  console.log('MongoDB connected!');
+  app.listen(8080);
+  }
+ )
+ .catch(err => console.log(err));
